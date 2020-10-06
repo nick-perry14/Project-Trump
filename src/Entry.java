ad336025a9b2825fc6de58bd1682c0f76399a61a
Nick Perry <nap14hockey@gmail.com>
2020-10-06 12:55:00 -0400

    Created Category Class

---
+++ src/Entry.java
@@ -0,0 +1,41 @@
+
+public class Entry {
+private String name;
+private String developer;
+private byte rating;
+private String category;
+
+public Entry(String name, String developer, byte rating, String category) {
+	this.name = name;
+	this.developer = developer;
+	this.rating = rating;
+	this.category = category;
+}
+
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getDeveloper() {
+	return developer;
+}
+public void setDeveloper(String developer) {
+	this.developer = developer;
+}
+public byte getRating() {
+	return rating;
+}
+public void setRating(byte rating) {
+	this.rating = rating;
+}
+public String getCategory() {
+	return category;
+}
+public void setCategory(String category) {
+	this.category = category;
+}
+
+
+}
