610050f731461a64c713685f965fc9216c578767
Nick Perry <nap14hockey@gmail.com>
2020-10-21 15:38:19 -0400

    Created Login Method.

--- src/user/User.java
+++ src/user/User.java
@@ -1,15 +1,20 @@
 package user;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class User {
-	private static ArrayList<User> users;
+	private static HashMap<String, User> users = new HashMap<>();
 	private String passwordHash;
+	private byte[] salt;
 	private String username;
 	private Ranks rank;
-	
+
 	public User(String username, String password) {
-		
+		this.username = username;
+		this.salt = PasswordHash.getSalt();
+		this.passwordHash = PasswordHash.hashPassword(password, this.salt);
+		this.rank = Ranks.User;
+		users.put(username, this);
 	}
 
 	/**
@@ -18,6 +23,29 @@ public class User {
 	public void loadUsers() {
 
 	}
-	
-	
+
+	/**
+	 * Login Method
+	 * 
+	 * @param username Username Given
+	 * @param password Password Given
+	 * @return User Object (or null if invalid login)
+	 */
+	public static User login(String username, String password) {
+		User user = users.get(username);
+		if (user == null)
+			return null;
+		if (PasswordHash.hashPassword(password, user.getSalt()).equals(user.getPassHash()))
+			return user;
+		return null;
+	}
+
+	private String getPassHash() {
+		return passwordHash;
+	}
+
+	private byte[] getSalt() {
+		return salt;
+	}
+
 }
