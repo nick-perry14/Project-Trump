f3914b8d16b2c31714c848ebee081d645c08dacb
Nick Perry <nap14hockey@gmail.com>
2020-10-21 16:59:06 -0400

    Allowed Ranks to serialize

--- src/com/brogrammers/projecttrump/user/Ranks.java
+++ src/com/brogrammers/projecttrump/user/Ranks.java
@@ -1,9 +1,12 @@
 package com.brogrammers.projecttrump.user;
+
+import java.io.Serializable;
+
 /**
  * Enum Of the various ranks.
  * @author Nick Perry
  *
  */
-enum Ranks {
+enum Ranks implements Serializable {
 User, Moderator, Admin
 }
